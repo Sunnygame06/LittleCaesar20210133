1ae21ff65dca7f6f25b1f7860e105198213ef815
Daniel Alvarado <20210133@ricaldone.edu.sv>
2026-04-20 15:33:42 -0600

    login y logout, cors

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,20 @@ import reviewsRoutes from "./src/routes/review.js"
 import customerRoutes from "./src/routes/customer.js"
 import registerCustomerRoutes from "./src/routes/registerCustomer.js"
 import registerEmployeeRoutes from "./src/routes/registerEmployee.js"
+import loginCustomerRoutes from "./src/routes/loginCustomer.js"
+import logoutCustomerRoutes from "./src/routes/logout.js"
+import cors from "cors";
 import cookieParser from "cookie-parser"
 
 //Crear una constante que el igual a
 //la libreria Express
 const app = express();
+
+app.use.cors({
+    origin: ["http://localhost:5173", "http//localhost:5174"],
+    //permitir el envio de cookies y credenciales
+    credentials: true
+})
 
 app.use(cookieParser());
 
@@ -23,6 +32,8 @@ app.use("/api/employee", employeesRoutes);
 app.use("/api/review", reviewsRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/registerCustomer", registerCustomerRoutes);
-app.use("/api/registerEmployee", registerEmployeeRoutes)
+app.use("/api/registerEmployee", registerEmployeeRoutes);
+app.use("/api/loginCustomers", loginCustomerRoutes);
+app.use("/api/logout", logoutCustomerRoutes);
 
 export default app;
