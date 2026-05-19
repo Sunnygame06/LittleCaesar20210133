30489415cf08888ee3136746957ca3155aa2f1a2
Josh1xs <m4valor@gmail.com>
2026-05-19 09:26:47 -0600

    Actualizacion de CRUD de Pizzas

--- backend/src/routes/pizza.js
+++ backend/src/routes/pizza.js
@@ -9,8 +9,21 @@ router.route("/")
 .get(pizzasController.getPizzas)
 .post(pizzasController.insertPizzas)
 
+router.route("/low-stock")
+.get(pizzasController.getLowStock)
+
+router.route("/price-range")
+.post(pizzasController.getPizzasByPriceRange)
+
+router.route("/count")
+.post(pizzasController.countPizzas)
+
+router.route("/search-name")
+.post(pizzasController.searchByName)
+
 router.route("/:id")
 .put(pizzasController.updatePizzas)
 .delete(pizzasController.deletePizzas)
+.get(pizzasController.getPizzasById)
 
 export default router
